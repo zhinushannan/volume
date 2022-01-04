b64c9474fcf1b94cb39071a07fe9faa4c28d7b09
zhinushannan <1377875184@qq.com>
2022-01-04 10:56:40 +0800

    02-Hadoop-新建Hadoop学习项目-HDFS： 1、在Hadoop上创建目录案例

--- 02-Hadoop/HDFSClient/src/main/java/club/kwcoder/hdfs/HdfsClient.java
+++ 02-Hadoop/HDFSClient/src/main/java/club/kwcoder/hdfs/HdfsClient.java
@@ -1,5 +1,14 @@
 package club.kwcoder.hdfs;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * 客户端代码常用步骤：
  * 1、获取一个客户端对象
@@ -8,6 +17,28 @@ package club.kwcoder.hdfs;
  */
 public class HdfsClient {
 
+    @Test
+    public void testMkDir() throws URISyntaxException, IOException, InterruptedException {
+        // 指定系统变量
+        System.setProperty("hadoop.home.dir", "F:\\hadoop-3.1.0");
 
+        // 链接的集群NameNode的地址
+        URI uri = new URI("hdfs://hadoop102:8020");
+        // 创建一个配置文件
+
+        Configuration configuration = new Configuration();
+
+        String user = "root";
+
+        // 1、获取客户端对象
+        FileSystem fs = FileSystem.get(uri, configuration, user);
+
+        // 2、执行相关操作
+        fs.mkdirs(new Path("/xiyou/huaguoshan"));
+
+        // 3、关闭资源
+        fs.close();
+
+    }
 
 }
