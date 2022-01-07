cc4173a94114e26ce4f9e53b5d4371e773dd159e
zhinushannan <1377875184@qq.com>
2022-01-07 16:04:20 +0800

    02-Hadoop-新建Hadoop学习项目-04-MapReduce.md-MapReduce序列化项目代码

---
+++ 02-Hadoop/MapReduceDemo/src/main/java/club/kwcoder/mapreduce/writable/FlowReducer.java
@@ -0,0 +1,30 @@
+package club.kwcoder.mapreduce.writable;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class FlowReducer extends Reducer<Text, FlowBean, Text, FlowBean> {
+
+    private final FlowBean outValue = new FlowBean();
+
+    @Override
+    protected void reduce(Text key, Iterable<FlowBean> values, Reducer<Text, FlowBean, Text, FlowBean>.Context context) throws IOException, InterruptedException {
+        long totalUp = 0;
+        long totalDown = 0;
+
+        for (FlowBean value : values) {
+            totalUp += value.getUpFlow();
+            totalDown += value.getDownFlow();
+        }
+
+        outValue
+                .setUpFlow(totalUp)
+                .setDownFlow(totalDown)
+                .setSumFlow();
+
+        context.write(key, outValue);
+    }
+
+}
